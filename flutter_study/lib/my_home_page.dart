976c866e64ce7455b4d9c7839cecea2089f2575d
RyukieSama <ryukie.sama@gmail.com>
2021-11-03 19:31:32 +0800

    MOD: <目录>

--- flutter_study/lib/my_home_page.dart
+++ flutter_study/lib/my_home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_study/container/my_container.dart';
-import 'package:flutter_study/row_column/my_row_column.dart';
-import 'text/my_list_widget.dart';
+import 'widget/container/my_container.dart';
+import 'widget/row_column/my_row_column.dart';
+import 'widget/text/my_list_widget.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
