f69f926a3c7afefe9d1cfa30b4aeb87b129b8e0b
RyukieSama <ryukie.sama@gmail.com>
2021-11-11 19:28:43 +0800

    ADD: <发现页Cell>

---
+++ fake_wechat/lib/widgets/moment/moment_home_cell.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class MomentHomeCell extends StatelessWidget {
+  final String title;
+  final String? subTitle;
+  final String imageName;
+  final String? subImageName;
+
+  MomentHomeCell(
+      {required this.title,
+      this.subTitle,
+      required this.imageName,
+      this.subImageName})
+      : assert(title != null, '标题不能为空'),
+        assert(imageName != null, '主图标不能为空');
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 44,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            color: Colors.green,
+            child: Row(
+              children: [
+                Image(image: AssetImage(imageName)),
+                const SizedBox(
+                  width: 8,
+                ),
+                Text(title),
+              ],
+            ),
+          ),
+          Container(
+              color: Colors.yellow,
+              child: Row(
+                children: [
+                  subTitle != null ? Text(subTitle!) : Container(),
+                  subImageName != null
+                      ? Image.asset(subImageName!)
+                      : Container(),
+                  Image.asset(
+                    'images/icon_right.png',
+                    width: 15,
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
