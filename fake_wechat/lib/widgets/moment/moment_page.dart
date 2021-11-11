5d68c7c226551627f8f0f72fe050ac4a314289ea
RyukieSama <ryukie.sama@gmail.com>
2021-11-11 18:15:50 +0800

    MOD: <AppBar> 一些设置

--- fake_wechat/lib/widgets/moment/moment_page.dart
+++ fake_wechat/lib/widgets/moment/moment_page.dart
@@ -10,7 +10,10 @@ class _MommentPageState extends State<MommentPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.cyan[200],
         title: const Text('发现'),
+        elevation: 0.0, // 导航底部阴影
+        centerTitle: true, // 为了安卓设置
       ),
       body: Container(
         color: Colors.brown,
