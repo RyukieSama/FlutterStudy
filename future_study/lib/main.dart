f216d4bc7797413f0034b5466c8123c9a03dd684
RyukieSama <ryukie.sama@gmail.com>
2021-11-27 17:25:44 +0800

    MOD: <多Future与Microtask>

--- future_study/lib/main.dart
+++ future_study/lib/main.dart
@@ -1,34 +1,32 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 
 int number = 1;
 
 void main() {
-  Future.wait([
-    Future(() {
-      sleep(Duration(seconds: 1));
-      print('${DateTime.now()}: 1');
-      return "任务1";
-    }),
-    Future(() {
-      sleep(Duration(seconds: 1));
-      print('${DateTime.now()}: 2');
-      return "任务2";
-    }),
-    Future(() {
-      sleep(Duration(seconds: 1));
-      print('${DateTime.now()}: 3');
-    }),
-    Future(() {
-      sleep(Duration(seconds: 1));
-      print('${DateTime.now()}: 4');
-    }),
-    Future(() {
-      sleep(Duration(seconds: 1));
-      print('${DateTime.now()}: 5');
-      return "任务5";
-    }),
-  ]).then((value) {
-    print(value);
+  Future((){
+    print('1');
+  }).then((value){
+    print('2');
+    scheduleMicrotask((){
+      print('3');
+    });
+  }).then((value){
+    print('4');
   });
+
+  scheduleMicrotask((){
+    print('5');
+  });
+
+  Future((){
+    print('6');
+  });
+
+  scheduleMicrotask((){
+    print('7');
+  });
+
+  print('8');
 }
