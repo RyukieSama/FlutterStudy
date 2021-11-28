8305216c3fd36457892ded7362cf3f26edbdbcf7
RyukieSama <ryukie.sama@gmail.com>
2021-11-28 10:26:39 +0800

    ADD: <Isolate> 资源竞争

--- future_study/lib/main.dart
+++ future_study/lib/main.dart
@@ -8,14 +8,22 @@ int number = 1;
 void main() {
   print('${DateTime.now()}: 1');
 
-  Isolate.spawn(isolateFunc, 15);
+  Isolate.spawn(isolateFunc, 1);
+  Isolate.spawn(isolateFunc, 2);
+  Isolate.spawn(isolateFunc, 3);
+  Isolate.spawn(isolateFunc, 4);
+  Isolate.spawn(isolateFunc, 5);
+  Isolate.spawn(isolateFunc, 6);
+  Isolate.spawn(isolateFunc, 7);
+  Isolate.spawn(isolateFunc, 8);
+  Isolate.spawn(isolateFunc, 9);
+  Isolate.spawn(isolateFunc, 10);
 
-
-  sleep(Duration(seconds: 3));
-  print('${DateTime.now()}: Sleep end');
+  sleep(Duration(seconds: 5));
+  print('${DateTime.now()}: Sleep end，number = $number');
 }
 
 void isolateFunc(int value) {
-  sleep(Duration(seconds: 1));
-  print('${DateTime.now()}: isolateFunc end $value');
+  number += 1;
+  print('${DateTime.now()}: 第$value个调用， number = $number');
 }
