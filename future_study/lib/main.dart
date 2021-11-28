9b9e16b266bd4d8873a09fd4f85537640fc4cfa2
RyukieSama <ryukie.sama@gmail.com>
2021-11-28 10:58:41 +0800

    ADD: <compute>

--- future_study/lib/main.dart
+++ future_study/lib/main.dart
@@ -1,29 +1,18 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:isolate';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-int number = 1;
+int number = 0;
 
 void main() async {
-  print('${DateTime.now()}: 1');
+  number = await compute(comFunc, 1);
 
-  // 创建端口
-  ReceivePort port = ReceivePort();
-  // Isolate
-  Isolate iso = await Isolate.spawn(isolateFunc, port.sendPort);
-  // 监听数据变化
-  port.listen((message) {
-    number = message;
-    print('${DateTime.now()}: 监听到 number = $number');
-    // 关闭端口
-    port.close();
-    // 销毁 Isolate
-    iso.kill();
-  });
+  print(number);
 }
 
-void isolateFunc(SendPort send) {
-  sleep(Duration(seconds: 3));
-  send.send(15);
+int comFunc(int value) {
+  sleep(Duration(seconds: 2));
+  return number + value;
 }
