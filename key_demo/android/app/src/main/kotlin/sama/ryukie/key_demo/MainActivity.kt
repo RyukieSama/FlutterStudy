df01a0219242108044a5035642cbb9a110cbbfde
RyukieSama <ryukie.sama@gmail.com>
2021-11-30 19:36:54 +0800

    ADD: <KeyDemo>

---
+++ key_demo/android/app/src/main/kotlin/sama/ryukie/key_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package sama.ryukie.key_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ key_demo/lib/main.dart
@@ -0,0 +1,81 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //key的作用就非常大了！！
+  List<Widget> items = [
+    ColorItem('第1个'),
+    ColorItem('第2个'),
+    ColorItem('第3个'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Key的作用'),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: items,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            items.removeAt(0);
+          });
+        },
+      ),
+    );
+  }
+}
+
+class ColorItem extends StatefulWidget {
+  final String title;
+
+  ColorItem(this.title, {Key? key}) : super(key: key);
+
+  @override
+  _ColorItemState createState() => _ColorItemState();
+}
+
+class _ColorItemState extends State<ColorItem> {
+  final color = Color.fromRGBO(
+      Random().nextInt(256), Random().nextInt(256), Random().nextInt(256), 1.0);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      child: Text(widget.title),
+      color: color,
+    );
+  }
+}
